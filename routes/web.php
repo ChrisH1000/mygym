604bd541a4d76953c3972b8db0a53ad568a2d537
Chris Hill <christopher.hill@ecu.edu.au>
2024-05-01 14:15:57 +0800

    Seeding, factories and relationships

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BookingController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ScheduledClassController;
@@ -19,9 +20,22 @@ Route::resource('/instructor/schedule', ScheduledClassController::class)
 ->only(['index', 'create', 'store', 'destroy'])
 ->middleware(['auth', 'role:instructor']);
 
-Route::get('/member/dashboard', function () {
-    return view('member.dashboard');
-})->middleware(['auth', 'role:member'])->name('member.dashboard');
+// Route::get('/member/dashboard', function () {
+//     return view('member.dashboard');
+// })->middleware(['auth', 'role:member'])->name('member.dashboard');
+
+// Route::get('/member/book', [BookingController::class, 'create'])->middleware(['auth', 'role:member'])->name('booking.create');
+
+Route::middleware(['auth', 'role:member'])->group(function() {
+    Route::get('/member/dashboard', function () {
+        return view('member.dashboard');
+    })->name('member.dashboard');
+
+    Route::get('/member/book', [BookingController::class, 'create'])->name('booking.create');
+    Route::post('/member/bookings', [BookingController::class, 'store'])->name('booking.store');
+    Route::get('/member/bookings', [BookingController::class, 'index'])->name('booking.index');
+    Route::delete('/member/bookings/{id}', [BookingController::class, 'destroy'])->name('booking.destroy');
+});
 
 Route::get('/admin/dashboard', function () {
     return view('admin.dashboard');
